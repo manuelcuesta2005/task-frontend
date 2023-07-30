73b3d40f42089065e1dd670e677a79040401c73d
manuelcuesta2005 <macuba2005@gmail.com>
2023-07-30 00:10:51 -0500

    segundo commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import React from 'react';
-import Login from '@/pages/Login';
 import Home from '@/pages/Home'
+import Login from '@/pages/Login';
 import Register from '@/pages/Register';
+import Editprofile from '@/pages/Edit-profile';
 import Recover_password from '@/pages/Recover-password'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
@@ -15,6 +16,7 @@ export default function App(){
             <Route path='/Register' element={<Register />} />
             <Route path='/Recover-password' element={<Recover_password />} />
             <Route path='/Home' element={<Home/>} />
+            <Route path='/Edit-profile' element={<Editprofile/>} />
         </Routes>
     </BrowserRouter>
     </div>
